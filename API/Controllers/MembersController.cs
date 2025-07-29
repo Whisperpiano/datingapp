b21646e4f6cb088b8a0a72de4df2347023be78aa
Whisperpiano <jesusalberola90@gmail.com>
2025-07-29 10:41:00 +0200

    First commit

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class MembersController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await context.Users.ToListAsync();
+        return members;
+    }
+    
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
+        
+        if (member == null) return NotFound();
+        
+        return member;
+    }
+    
+}
