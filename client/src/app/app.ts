7c8f5323dc69ede9938d9cec0cba865c06c837f6
Whisperpiano <jesusalberola90@gmail.com>
2025-08-01 10:04:35 +0200

    feat(client): enhance navigation and routing with guards, services, and splash screen

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,47 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { AccountService } from '../core/services/account-service';
-import { lastValueFrom } from 'rxjs';
-import { Home } from '../features/home/home';
-import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = signal('Dating App');
-  protected members = signal<User[]>([]);
-
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-
-    if (!userString) {
-      return;
-    }
-
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(
-        this.http.get<User[]>('https://localhost:5001/api/members')
-      );
-    } catch (error) {
-      console.error('Error fetching members:', error);
-      throw error;
-    }
-  }
+export class App {
+  protected router = inject(Router);
 }
